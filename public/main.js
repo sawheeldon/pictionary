ccf77b0281c5327b78050a4b58d73cb48d9ea43f
Simon <simon@jampot.design>
2016-08-30 23:58:44 +0000

    REPLACE-ME

---
+++ public/main.js
@@ -0,0 +1,37 @@
+var socket = io();
+
+var pictionary = function() {
+    var canvas, context;
+
+    var draw = function(position) {
+        context.beginPath();
+        context.arc(position.x, position.y,
+                         6, 0, 2 * Math.PI);
+        context.fill();
+    };
+
+    canvas = $('canvas');
+    context = canvas[0].getContext('2d');
+    canvas[0].width = canvas[0].offsetWidth;
+    canvas[0].height = canvas[0].offsetHeight;
+    canvas.on('mousemove', function(event) {
+        var offset = canvas.offset();
+        var position = {x: event.pageX - offset.left,
+                        y: event.pageY - offset.top};
+        draw(position);
+    });
+};
+
+socket.on('connect', function(draw) {
+    var draw = document.on.mousemove;
+     if (draw) {
+         socket.emit('draw', function(position){
+             
+         });
+     }
+     
+     });
+
+$(document).ready(function() {
+    pictionary();
+});
