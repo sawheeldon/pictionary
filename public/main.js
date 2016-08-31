073c16d004feb6e022cf300c32069e692196c42a
Simon <simon@jampot.design>
2016-08-31 14:26:46 +0000

    mouseover and up event fine, drawing goes on multiple tabs

--- public/main.js
+++ public/main.js
@@ -1,7 +1,10 @@
 var socket = io();
 
+var incognitoMode = false;
+
 var pictionary = function() {
-    var canvas, context;
+    
+    var canvas, context, drawing;
 
     var draw = function(position) {
         context.beginPath();
@@ -14,23 +17,33 @@ var pictionary = function() {
     context = canvas[0].getContext('2d');
     canvas[0].width = canvas[0].offsetWidth;
     canvas[0].height = canvas[0].offsetHeight;
+     canvas.on('mousedown', function() {
+        if (draw) {
+            drawing = true;
+        }
+    });
+    canvas.on('mouseup', function() {
+        drawing = false;
+    });
     canvas.on('mousemove', function(event) {
         var offset = canvas.offset();
-        var position = {x: event.pageX - offset.left,
-                        y: event.pageY - offset.top};
-        draw(position);
-    }); 
+        var position = {
+            x: event.pageX - offset.left,
+            y: event.pageY - offset.top
+        };
+        if (drawing) {
+            if (!incognitoMode) {
+                draw(position);
+            }
+            socket.emit('draw', position);
+        }
+    });
+    
+    
+     socket.on('draw', draw);
+    
+    
 };
-
-socket.on('connect', function(draw) {
-    var draw = document.on.mousemove;
-     if (draw) {
-         socket.emit('draw', function(position){
-             
-         });
-     }
-     
-     });
 
 $(document).ready(function() {
     pictionary();
--- server.js
+++ server.js
@@ -8,4 +8,13 @@ app.use(express.static('public'));
 var server = http.Server(app);
 var io = socket_io(server);
 
+var drawer = null;
+
+
+io.on('connect', function(socket) {
+    socket.on('draw', function(position) {
+        socket.broadcast.emit('draw', position);
+    });
+});
+
 server.listen(process.env.PORT || 8080);
